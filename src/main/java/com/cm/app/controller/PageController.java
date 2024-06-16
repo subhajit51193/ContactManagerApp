2ce89a946cf2499adbbdd7dbac8bb088ad5afadb
subhajit51193 <nnorth87@gmail.com>
2024-06-16 21:48:13 +0530

    basic html file added

--- src/main/java/com/cm/app/controller/PageController.java
+++ src/main/java/com/cm/app/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.cm.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageController {
 
 	/**
-	 * 
+	 * Basic Home page layout
 	 * @return
 	 */
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		//sending data to view
+		model.addAttribute("name", "Substring technologies");
+		model.addAttribute("YT", "Kaizen");
+		model.addAttribute("git", "https://github.com/subhajit51193");
 		System.out.println("Home Page");
 		return "home";
 	}
